572639ef37eb9b2c0b37a2a0287e98b50e6ef47a
Jorge Arrieta <jorge.r.arrieta.jr@gmail.com>
2020-12-20 22:03:30 -0600

    Adding comments

--- foxnews_scrapper.py
+++ foxnews_scrapper.py
@@ -1,17 +1,19 @@
-from bs4 import BeautifulSoup
-import requests
+from bs4 import BeautifulSoup   # importing beautiful soup module
+import requests     # importing requests module
 
-source = requests.get('https://www.foxnews.com/').text
-soup = BeautifulSoup(source, 'html.parser')
+source = requests.get('https://www.foxnews.com/').text    # getting info from source
+soup = BeautifulSoup(source, 'html.parser')   # parsing data from source
 
-for article in soup.find_all("article"):
+for article in soup.find_all("article"):     # running a loop to all articles
 
+    # gathering headline information
     headline = article.h2.text
     print()
     print("Headline: ")
     print(headline)
     print()
 
+    # gathering video information
     video = article.a
     print()
     print("Video: ")
