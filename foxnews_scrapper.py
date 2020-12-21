5ed14532d769edd3291ca2a1e4d6a1e7e8806009
Jorge Arrieta <jorge.r.arrieta.jr@gmail.com>
2020-12-20 21:37:59 -0600

    Adding some formatting for the output

--- foxnews_scrapper.py
+++ foxnews_scrapper.py
@@ -7,7 +7,14 @@ soup = BeautifulSoup(source, 'html.parser')
 article = soup.find("article")
 
 headline = article.h2.text 
+print()
+print("Headline: ")
 print(headline)
+print()
 
 video = article.a 
+print()
+print("Video: ")
 print(video.get('href'))
+print()
+print("===========================")
