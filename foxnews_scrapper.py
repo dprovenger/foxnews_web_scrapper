90400ef9d411a08ae1cb955cb21237c0c0222640
Jorge Arrieta <jorge.r.arrieta.jr@gmail.com>
2020-12-20 21:34:24 -0600

    Pulling headline details

--- foxnews_scrapper.py
+++ foxnews_scrapper.py
@@ -5,4 +5,6 @@ source = requests.get('https://www.foxnews.com/').text
 soup = BeautifulSoup(source, 'html.parser')
 
 article = soup.find("article")
-print(article)
+
+headline = article.h2.text 
+print(headline)
