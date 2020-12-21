bce0aece7593880f0bcab01f176dbed9620a2748
Jorge Arrieta <jorge.r.arrieta.jr@gmail.com>
2020-12-20 21:39:53 -0600

    Looping to get all the articles from foxnews.com

--- foxnews_scrapper.py
+++ foxnews_scrapper.py
@@ -4,17 +4,17 @@ import requests
 source = requests.get('https://www.foxnews.com/').text
 soup = BeautifulSoup(source, 'html.parser')
 
-article = soup.find("article")
+for article in soup.find_all("article"):
 
-headline = article.h2.text 
-print()
-print("Headline: ")
-print(headline)
-print()
+    headline = article.h2.text
+    print()
+    print("Headline: ")
+    print(headline)
+    print()
 
-video = article.a 
-print()
-print("Video: ")
-print(video.get('href'))
-print()
-print("===========================")
+    video = article.a
+    print()
+    print("Video: ")
+    print(video.get('href'))
+    print()
+    print("===========================")
